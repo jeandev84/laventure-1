847d71fc674f31180e1bac53426cd3b177b4406d
jeandev84 <jeandev84@yahoo.com>
2022-03-03 16:31:54 +0300

    change database configuration

--- config/params/database.php
+++ config/params/database.php
@@ -1,48 +1,25 @@
 <?php
 
+/*
+|------------------------------------------------------------------
+|     CONNECTION TO DATABASE [ mysql, sqlite, pgsql ]
+|------------------------------------------------------------------
+*/
+
+
 return [
-
-    /*
-    |------------------------------------------------------------------
-    |     CONNECTION TO DATABASE [ mysql, sqlite, pgsql, oci (oracle) ]
-    |------------------------------------------------------------------
-    */
-
     'connection' => env('DB_TYPE', 'mysql'),
-    'sqlite' => [
-        'driver'   => 'sqlite',
-        'database' => '../database/laventure.db',
-        'options'  => []
+    'database'   => env('DB_NAME', 'laventure'),
+    'host'       => env('DB_HOST', '127.0.0.1'),
+    'port'       => env('DB_PORT', '3306'),
+    'username'   => env('DB_USER', 'root'),
+    'password'   => env('DB_PASS', ''),
+    'collation'  => 'utf8_unicode_ci',
+    'charset'    => 'utf8',
+    'prefix'     => '',
+    'engine'     => 'MyISAM', // InnoDB
+    'options'    => [
+        'commands' => 'SET SQL_MODE=ANSI_QUOTES'
     ],
-    'mysql' => [
-        'driver'     => 'mysql',
-        'database'   => env('DB_NAME', 'laventure'),
-        'host'       => env('DB_HOST', '127.0.0.1'),
-        'port'       => env('DB_PORT', '3306'),
-        'username'   => env('DB_USER', 'root'),
-        'password'   => env('DB_PASS', ''),
-        'collation'  => 'utf8_unicode_ci',
-        'charset'    => 'utf8',
-        'prefix'     => '',
-        'engine'     => 'MyISAM', // InnoDB
-        'options'    => [
-            'commands' => 'SET SQL_MODE=ANSI_QUOTES'
-        ],
-    ],
-    'pgsql' => [
-        'driver'     => 'pgsql',
-        'database'   => env('DB_NAME', 'laventure'),
-        'host'       => env('DB_HOST', '127.0.0.1'),
-        'port'       => env('DB_PORT', '5432'),
-        'username'   => env('DB_USER', 'postgres'),
-        'password'   => env('DB_PASS', '123456'),
-        'collation'  => 'utf8_unicode_ci',
-        'charset'    => 'utf8',
-        'prefix'     => '',
-        'engine'     => 'InnoDB', // MyISAM
-        'options'    => [
-            // 'commands' => 'SET SQL_MODE=ANSI_QUOTES'
-        ],
-    ],
-    'migration_path' => 'database/migrations/'
+    'migration_path' => 'database/migrations/' // specific for model
 ];
